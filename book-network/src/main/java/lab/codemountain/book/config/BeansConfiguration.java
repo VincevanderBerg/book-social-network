c0a8cb4e14addff1719c5f49d1dfd26da2c15339
vincevanderberg <vincevanderberg@gmail.com>
2024-06-18 15:10:41 +0200

    feat: Implement auditing aware feature
    - Enabled JPA auditing with custom AuditorAware implementation in BookNetworkApiApplication
    - Created ApplicationAuditAware class to provide the current auditor's ID based on authenticated user
    - Added auditorAware bean in BeansConfiguration to integrate the custom auditor provider

--- book-network/src/main/java/lab/codemountain/book/config/BeansConfiguration.java
+++ book-network/src/main/java/lab/codemountain/book/config/BeansConfiguration.java
@@ -3,6 +3,7 @@ package lab.codemountain.book.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -24,6 +25,11 @@ public class BeansConfiguration {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
 
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuditorAware<Long> auditorAware() {
+        return new ApplicationAuditAware();
     }
 
     @Bean
